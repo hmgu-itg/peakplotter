38735c1429a0a923ff2bc5e47fc796e247f8f588
Young-Chan Park <young.chan.park93@gmail.com>
2021-05-14 02:12:06 +0900

    Added function to check existence of locuszoom data files

--- peakplotter/utils.py
+++ peakplotter/utils.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+from __future__ import annotations
 
 import shutil
+from pathlib import Path
+
+from .errors import MissingExecutableError
 
 DEPENDENT_EXECUTABLES = [
     'tabix', # HTSLib
@@ -26,5 +30,18 @@ def check_executable(executable: str) -> bool:
     return bool(shutil.which(executable))
 
 
+
+def _get_locuszoom_data_path() -> tuple[Path, Path]:
+    locuszoom_path = shutil.which('locuszoom')
+    if locuszoom_path is None:
+        raise MissingExecutableError('locuszoom executable not in PATH')
+    locuszoom_data = locuszoom_path.parent.parent.joinpath('data')
+    recomb_file = locuszoom_data.joinpath('recomb_rate_b38.txt')
+    ref_file = locuszoom_data.joinpath('refFlat_b38.txt')
+
+    if not all([recomb_file.exists(), ref_file.exists()]):
+        raise FileNotFoundError('Locuszoom data not found')
+
+    return recomb_file, ref_file
 
 
