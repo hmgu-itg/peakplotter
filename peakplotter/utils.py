d58d196a51fc85b55d28919e22bd96c4cde6a898
Young-Chan Park <young.chan.park93@gmail.com>
2021-05-13 14:50:56 +0900

    Added utils.py

---
+++ peakplotter/utils.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import shutil
+
+DEPENDENT_EXECUTABLES = [
+    'tabix', # HTSLib
+    'locuszoom', # Locuszoom
+    'dbmeister.py', # Locuszoom
+    'plink', # Plink
+    'sponge' # Moreutils
+]
+
+
+def check_executable(executable: str) -> bool:
+    '''
+    Checks whether executable exists in PATH.
+
+    Example
+    -------
+    >>> check_executable('cd')
+    True
+    >>> check_executable('non-existant-executable')
+    False
+    '''
+    return bool(shutil.which(executable))
+
+
+
+
