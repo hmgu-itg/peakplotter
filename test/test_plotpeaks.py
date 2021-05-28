1281541d733de4818128d7c7f291254e50ad59bf
Young-Chan Park <young.chan.park93@gmail.com>
2021-05-28 13:00:49 +0900

    Added first test for plotpeaks.py

---
+++ test/test_plotpeaks.py
@@ -0,0 +1,36 @@
+import io 
+
+import pandas as pd
+
+from peakplotter import plotpeaks
+
+
+def test_read_assoc_with_METAL_output():
+    """
+    METAL outputs have 'nanenan' values in p-value columns. Exclude these.
+    
+    Note
+    ----
+    1. https://genome.sph.umich.edu/wiki/METAL_Documentation
+    """
+    chr_col, pos_col, pval_col, maf_col, rs_col, a1_col, a2_col = 'c', 'pos', 'pval', 'maf', 'rs', 'a1', 'a2'
+    example = pd.DataFrame([
+            [1, 'rs001', 100, 'G', 'T', 0.01, 'nanenan'],
+            [1, 'rs002', 200, 'G', 'T', 0.01, '0.01']
+        ]
+        , columns = [chr_col, rs_col, pos_col, a1_col, a2_col, maf_col, pval_col]
+    )
+
+    example_buff = io.StringIO()
+    example.to_csv(example_buff, sep = '\t', header = True, index = False)
+    example_buff.seek(0)
+    iterable = plotpeaks.read_assoc(example_buff, chr_col, pos_col, pval_col, maf_col, rs_col, a1_col, a2_col)
+    data = next(iterable)
+
+    expected = pd.DataFrame([
+            [1, 'rs002', 200, 'G', 'T', 0.01, 0.01]
+        ]
+        , columns = [chr_col, rs_col, pos_col, a1_col, a2_col, maf_col, pval_col]
+    )
+    
+    pd.testing.assert_frame_equal(expected, data)
