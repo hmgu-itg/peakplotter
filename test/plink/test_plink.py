a9009f86224bf07c414aa1866854f23fe3e01237
Young-Chan Park <young.chan.park93@gmail.com>
2021-05-29 16:40:33 +0900

    Moved plink test files and added plink tests

--- test/plink/test_plink.py
+++ test/plink/test_plink.py
@@ -13,7 +13,7 @@ def setup():
 # TODO: Add cleanup
 def test_exclude_all_variants_raise_non_zero():
     plink = setup()
-    bfile = exclude_dir.joinpath('cohortA')
+    bfile = base.joinpath('cohortA')
     excludelist = exclude_dir.joinpath('excludelist')
     out = exclude_dir.joinpath('exclude_all_test')
 
@@ -31,8 +31,8 @@ def test_plink_exclude_across_bfiles():
     is therefore expected to be a `[Path(f'{cohortB_bfile}.excluded')]`.
     """
     plink = setup()
-    cohortA_bfile = exclude_dir.joinpath('cohortA')
-    cohortB_bfile = exclude_dir.joinpath('cohortB')
+    cohortA_bfile = base.joinpath('cohortA')
+    cohortB_bfile = base.joinpath('cohortB')
     excludelist = exclude_dir.joinpath('excludelist')
 
     bfiles = [cohortA_bfile, cohortB_bfile]
@@ -40,3 +40,29 @@ def test_plink_exclude_across_bfiles():
     expected = [Path(f'{cohortB_bfile}.excluded')]
 
     assert output == expected
+
+
+def test_plink_merge_one_cohort():
+    """
+    Check that plink just copies the binary file when executing
+    merge command with one bfile in mergelist
+    """
+    plink = setup()
+    mergelist = merge_dir.joinpath('mergelist_one_cohort.txt')
+    out = merge_dir.joinpath('merge_one_cohort')
+    output = plink.merge(mergelist, out)
+    
+    assert output.returncode == 0, f'Merging one cohort returned exit code {output.returncode}'
+
+def test_plink_merge_two_cohorts():
+    """
+    Check that plink just copies the binary file when executing
+    merge command with one bfile in mergelist
+    """
+    plink = setup()
+    mergelist = merge_dir.joinpath('mergelist_two_cohorts.txt')
+    out = merge_dir.joinpath('merge_two_cohorts')
+    output = plink.merge(mergelist, out)
+    
+    assert output.returncode == 0, f'Merging one cohort returned exit code {output.returncode}'
+
