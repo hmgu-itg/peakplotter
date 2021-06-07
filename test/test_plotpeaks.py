63b275eb4621fcae97f2a0ea33603c7ccfcdad49
Young-Chan Park <young.chan.park93@gmail.com>
2021-06-07 20:54:15 +0900

    Added test for dealing with invalid allele string value

--- test/test_plotpeaks.py
+++ test/test_plotpeaks.py
@@ -36,6 +36,31 @@ def test_read_assoc_with_METAL_output():
     pd.testing.assert_frame_equal(expected, data)
 
 
+
+def test_read_assoc_with_invalid_allele_string():
+    chr_col, pos_col, pval_col, maf_col, rs_col, a1_col, a2_col = 'c', 'pos', 'pval', 'maf', 'rs', 'a1', 'a2'
+    example = pd.DataFrame([
+            [1, 'rs001', 100, 'G', 'T_INVALID', 0.01, 0.01],
+            [1, 'rs002', 200, 'G', 'T', 0.01, 0.01]
+        ]
+        , columns = [chr_col, rs_col, pos_col, a1_col, a2_col, maf_col, pval_col]
+    )
+
+    example_buff = io.StringIO()
+    example.to_csv(example_buff, sep = '\t', header = True, index = False)
+    example_buff.seek(0)
+    iterable = plotpeaks.read_assoc(example_buff, chr_col, pos_col, pval_col, maf_col, rs_col, a1_col, a2_col)
+    data = next(iterable)
+
+    expected = pd.DataFrame([
+            [1, 'rs002', 200, 'G', 'T', 0.01, 0.01]
+        ]
+        , columns = [chr_col, rs_col, pos_col, a1_col, a2_col, maf_col, pval_col]
+    )
+
+    pd.testing.assert_frame_equal(expected, data)
+
+
 def test_get_signals_when_no_signif_signals():
     """
     All detected signal's p-value is greater than significance threshold.
