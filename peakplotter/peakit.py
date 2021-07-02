06353860ea11421fec391fe6c24af3fa06d4cc7c
Young-Chan Park <young.chan.park93@gmail.com>
2021-07-03 01:53:01 +0900

    Modified Peak and PeakCollections class

--- peakplotter/peakit.py
+++ peakplotter/peakit.py
@@ -5,91 +5,87 @@ from io import StringIO
 import pandas as pd
 from pybedtools import BedTool
 
-MARGIN = 500000
 
 class Peak:
-	chrom = 0
-	start = 0
-	end = 0
-	def __init__(self, chr, ps):
-		self.chrom = chr
-		self.start = ps
-		self.end = ps
+    def __init__(self, chrom: int, start: int, end: int):
+        self.chrom = chrom
+        self.start = start
+        self.end = end
 
-	def add_snp(self, chr, ps):
-		if ps < self.start:
-			self.start = ps
-		if ps > self.end:
-			self.end = ps
+    def add_snp(self, chrom: int, pos: int):
+        """
+        Extend the peak region up to the input SNP's position. 
+        
+        Raises
+        ------
+        ValueError
+            If the input chromosome is different from the peak's chromosome
+        """
+        if self.chrom != chrom:
+            raise ValueError("Input SNP's chromosome different from the peak's chromosome")
+        
+        if pos < self.start:
+            self.start = pos
+        if pos > self.end:
+            self.end = pos
+    
+    def __repr__(self):
+        return f'Peak({self.chrom}, {self.start}, {self.end})'
+    
+
+class PeakCollection(list):
+    def __init__(self, MARGIN = 500_000):
+        self.MARGIN = MARGIN
+        
+    def check_and_add(self, chrom, pos):
+        if len(self)==0:
+            peak = Peak(chrom, pos, pos)
+            self.append(peak)
+            return
+
+        found = False
+        for i, peak in enumerate(self):
+            if (chrom == peak.chrom) and ((peak.start - self.MARGIN) < pos < (peak.end + self.MARGIN)):
+                self[i].add_snp(chrom, pos)
+                found = True
+
+        if not found:
+            self.append(Peak(chrom, pos, pos))
+            
+    def extend_peaks(self, length: int):
+        """
+        Extend the region around its center so that it spans `length`.
+        If it is already `length` or larger, add 1/4 `length` either side
+        """
+        for i, peak in enumerate(self):
+            if peak.end - peak.start < length:
+                center = peak.start + (peak.end - peak.start)/2
+                peak.start = int(center - length/2)
+                peak.end = int(center + length/2)
+            else:
+                peak.start = int(peak.start - length/4)
+                peak.end = int(peak.end + length/4)
+
+            if peak.start < 0:
+                peak.start = 0
+                
+    @property
+    def data(self):
+        d = list()
+        for peak in self:
+            assert peak.start < peak.end
+            d.append([peak.chrom, peak.start, peak.end])
+        d = pd.DataFrame(d, columns = ['chrom', 'start', 'end']).sort_values(['chrom', 'start']).reset_index(drop=True)
+        return d
 
 
-class peakCollection:
-	def __init__(self):
-		self.peaks = []
-
-	def check_and_add(self, chr, ps):
-		if len(self.peaks)==0:
-			x = Peak(chr, ps)
-			self.peaks = [x]
-			return
-		
-		found = 0
-		for i, peak in enumerate(self.peaks):
-			if (chr == peak.chrom) and ((peak.start - MARGIN) < ps < (peak.end + MARGIN)):
-				self.peaks[i].add_snp(chr, ps)
-				found=1
-		
-		if not found:
-			self.peaks.append(Peak(chr, ps))
-
-		
-	def print(self):
-		for peak in self.peaks:
-			print(str(peak.chrom)+"\t"+str(peak.start)+"\t"+str(peak.end));
-	
-	@property
-	def data(self):
-		d = list()
-		for peak in self.peaks:
-			assert peak.start < peak.end
-			d.append([peak.chrom, peak.start, peak.end])
-		d = pd.DataFrame(d, columns = ['chrom', 'start', 'end']).sort_values(['chrom', 'start']).reset_index(drop=True)
-		return d
-	
-	def extend(self, TOTAL_LENGTH):
-		## extend the region around its center so that it spans TOTAL_LENGTH
-		## If it is already TOTAL_LENGTH or larger, add 1/4 TOTAL_LENGTH either side
-		for i,peak in enumerate(self.peaks):
-			if peak.end - peak.start < TOTAL_LENGTH:
-				center=peak.start+ (peak.end - peak.start)/2
-				peak.start=int(center - TOTAL_LENGTH/2)
-				peak.end=int(center + TOTAL_LENGTH/2)
-			else:
-				peak.start=int(peak.start-TOTAL_LENGTH/4)
-				peak.end=int(peak.end+TOTAL_LENGTH/4)
-			
-			if peak.start<0:
-				peak.start=0
-			self.peaks[i]=peak
-
-def peakit(signals, pvalcol, chrcol, pscol):
-	d = pd.read_table(signals)
-	d.sort_values(pvalcol, inplace=True)
-
-	p = peakCollection()
-	for index, row in d.iterrows():
-		p.check_and_add(row[chrcol], row[pscol])
-
-	p.extend(1000000)
-	p.print()
-
-def _peakit(signals: pd.DataFrame, pvalcol: str, chrcol: str, pscol: str) -> peakCollection:
-	sorted_signals = signals.sort_values(pvalcol)
-	p = peakCollection()
-	for index, row in sorted_signals.iterrows():
-		p.check_and_add(row[chrcol], row[pscol])
-	p.extend(1000000)
-	return p
+def peakit(signals: pd.DataFrame, pval_col: str, chr_col: str, pos_col: str) -> PeakCollection:
+    sorted_signals = signals.sort_values(pval_col)
+    peaks = PeakCollection()
+    for index, row in sorted_signals.iterrows():
+        peaks.check_and_add(row[chr_col], row[pos_col])
+    peaks.extend_peaks(1_000_000)
+    return peaks
 
 
 def bedtools_merge(data: pd.DataFrame) -> pd.DataFrame:
@@ -102,4 +98,4 @@ def bedtools_merge(data: pd.DataFrame) -> pd.DataFrame:
 
 
 if __name__ == '__main__':
-	peakit(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    peakit(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
