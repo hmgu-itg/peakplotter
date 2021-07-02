ffdf402905a5521e3d9077c85029f9a5f7a3f7dc
Young-Chan Park <young.chan.park93@gmail.com>
2021-07-03 03:34:58 +0900

    Added multiple PeakCollection.merge tests

--- test/test_peakit.py
+++ test/test_peakit.py
@@ -32,7 +32,7 @@ def test_PeakCollection():
     
     assert len(peaks) == 2
 
-def test_PeakCollection_merge():
+def test_PeakCollection_merge_one_peak_to_merge():
     peaks = PeakCollection()
     peaks[:] = [
         Peak(1, 100, 200),
@@ -45,6 +45,53 @@ def test_PeakCollection_merge():
         Peak(1, 100, 300),
         Peak(2, 100, 200),
     ]
+
+    peaks.merge()
+
+    assert peaks == expected
+
+def test_PeakCollection_merge2_multiple_peaks_merge():
+    peaks = PeakCollection()
+    peaks[:] = [
+        Peak(22, 100, 200),
+        Peak(3, 150, 300),
+        Peak(1, 150, 300),
+        Peak(10, 300, 500),
+        Peak(1, 250, 500),
+        Peak(3, 200, 1000)
+    ]
+
+    expected = PeakCollection()
+    expected[:] = [
+        Peak(1, 150, 500),
+        Peak(3, 150, 1000),
+        Peak(10, 300, 500),
+        Peak(22, 100, 200)
+    ]
+
+    peaks.merge()
+
+    assert peaks == expected
+
+def test_PeakCollection_merge3_only_one_peak():
+    peaks = PeakCollection()
+    peaks[:] = [
+        Peak(1, 100, 200),
+    ]
+
+    expected = PeakCollection()
+    expected[:] = [
+        Peak(1, 100, 200),
+    ]
+
+    peaks.merge()
+
+    assert peaks == expected
+
+def test_PeakCollection_merge4_no_peak():
+    peaks = PeakCollection()
+
+    expected = PeakCollection()
 
     peaks.merge()
 
