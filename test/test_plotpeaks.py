766f5f6b2ffe9367163439798cac0ed6275b8725
Archit Singh <singharchit96@outlook.com>
2022-08-22 11:53:14 +0200

    unit_test_NA_bug

--- test/test_plotpeaks.py
+++ test/test_plotpeaks.py
@@ -85,3 +85,60 @@ def test_get_signals_when_no_signif_signals():
 
     signals = plotpeaks.get_signals(iterable, 5e-8, chr_col, pos_col, pval_col)
     assert signals.empty, 'Signals dataframe is not empty'
+
+
+def test_read_asoc_for_NA_values_in_pval_column():
+    """
+    summary statistics file with NA values
+    This is to check whether 'NA' value in the P-value column causes the read_assoc function to fail in reading the file.
+    Return dataframe with row omitted.
+    """
+    chr_col, pos_col, pval_col, maf_col, rs_col, a1_col, a2_col = 'c', 'pos', 'pval', 'maf', 'rs', 'a1', 'a2'
+    example = pd.DataFrame([
+            [1, 'rs001', 100, 'G', 'T', 0.01, 'NA'],
+            [1, 'rs002', 200, 'G', 'T', 0.01, 0.01]
+        ]
+        , columns = [chr_col, rs_col, pos_col, a1_col, a2_col, maf_col, pval_col]
+    )
+    example_buff = io.StringIO()
+    example.to_csv(example_buff, sep = '\t', header = True, index = False)
+    example_buff.seek(0)
+    logger = get_test_logger()
+    iterable = plotpeaks.read_assoc(example_buff, chr_col, pos_col, pval_col, maf_col, rs_col, a1_col, a2_col, logger)
+    data = next(iterable)
+    expected = pd.DataFrame([
+            [1, 'rs002', 200, 'G', 'T', 0.01, 0.01]
+        ]
+        , columns = [chr_col, rs_col, pos_col, a1_col, a2_col, maf_col, pval_col]
+    )
+
+    pd.testing.assert_frame_equal(expected, data)
+
+
+def test_read_asoc_for_NA_values_in_extra_column():
+    """
+    summary statistics file with NA values
+    This is to check whether 'NA' values in an extra column causes the read_assoc function to fail in reading the file.
+    Output is identical to input.
+    """
+    chr_col, pos_col, pval_col, maf_col, rs_col, a1_col, a2_col, extra = 'c', 'pos', 'pval', 'maf', 'rs', 'a1', 'a2','extra'
+    example = pd.DataFrame([
+            [1, 'rs001', 100, 'G', 'T', 0.001, 0.01, 'NA'],
+            [1, 'rs002', 200, 'G', 'T', 0.01, 0.01, 'NA']
+        ]
+        , columns = [chr_col, rs_col, pos_col, a1_col, a2_col, maf_col, pval_col, extra]
+    )
+    example_buff = io.StringIO()
+    example.to_csv(example_buff, sep = '\t', header = True, index = False)
+    example_buff.seek(0)
+    logger = get_test_logger()
+    iterable = plotpeaks.read_assoc(example_buff, chr_col, pos_col, pval_col, maf_col, rs_col, a1_col, a2_col, extra, logger)
+    data = next(iterable)
+    expected = pd.DataFrame([
+            [1, 'rs001', 100, 'G', 'T', 0.01, 0.01, 'NA'],
+            [1, 'rs002', 200, 'G', 'T', 0.01, 0.01, 'NA']
+        ]
+        , columns = [chr_col, rs_col, pos_col, a1_col, a2_col, maf_col, pval_col, extra]
+    )
+
+    pd.testing.assert_frame_equal(expected, data)
