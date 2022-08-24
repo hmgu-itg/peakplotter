f48d442c9ce28e60b453f175a5f7241dc6f9fe94
Archit Singh <singharchit96@outlook.com>
2022-08-24 17:39:47 +0200

    testing for omission of unused columns in input summary stats file

--- test/test_plotpeaks.py
+++ test/test_plotpeaks.py
@@ -115,16 +115,16 @@ def test_read_asoc_for_NA_values_in_pval_column():
     pd.testing.assert_frame_equal(expected, data)
 
 
-def test_read_asoc_for_NA_values_in_extra_column():
+def test_read_asoc_for_extra_columns():
     """
-    summary statistics file with NA values
-    This is to check whether 'NA' values in an extra column causes the read_assoc function to fail in reading the file.
-    Output is identical to input.
+    summary statistics file with unused column
+    This is to check whether unused column is removed when read_assoc function reads in a summary stats file.
+    Returns dataframe with omitted column.
     """
     chr_col, pos_col, pval_col, maf_col, rs_col, a1_col, a2_col, extra = 'c', 'pos', 'pval', 'maf', 'rs', 'a1', 'a2','extra'
     example = pd.DataFrame([
-            [1, 'rs001', 100, 'G', 'T', 0.001, 0.01, 'NA'],
-            [1, 'rs002', 200, 'G', 'T', 0.01, 0.01, 'NA']
+            [1, 'rs001', 100, 'G', 'T', 0.01, 0.01, 'NA'],
+            [1, 'rs002', 200, 'G', 'T', 0.01, 0.01, 1]
         ]
         , columns = [chr_col, rs_col, pos_col, a1_col, a2_col, maf_col, pval_col, extra]
     )
@@ -132,13 +132,13 @@ def test_read_asoc_for_NA_values_in_extra_column():
     example.to_csv(example_buff, sep = '\t', header = True, index = False)
     example_buff.seek(0)
     logger = get_test_logger()
-    iterable = plotpeaks.read_assoc(example_buff, chr_col, pos_col, pval_col, maf_col, rs_col, a1_col, a2_col, extra, logger)
+    iterable = plotpeaks.read_assoc(example_buff, chr_col, pos_col, pval_col, maf_col, rs_col, a1_col, a2_col, logger)
     data = next(iterable)
     expected = pd.DataFrame([
-            [1, 'rs001', 100, 'G', 'T', 0.01, 0.01, 'NA'],
-            [1, 'rs002', 200, 'G', 'T', 0.01, 0.01, 'NA']
+            [1, 'rs001', 100, 'G', 'T', 0.01, 0.01],
+            [1, 'rs002', 200, 'G', 'T', 0.01, 0.01]
         ]
-        , columns = [chr_col, rs_col, pos_col, a1_col, a2_col, maf_col, pval_col, extra]
+        , columns = [chr_col, rs_col, pos_col, a1_col, a2_col, maf_col, pval_col]
     )
 
-    pd.testing.assert_frame_equal(expected, data)
+    assert len(expected.columns) < len(data.columns)
