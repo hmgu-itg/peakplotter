7de311c9479de5e3fc3077aed8cf724d593797bc
Young-Chan Park <young.chan.park93@gmail.com>
2022-09-05 07:32:18 +0200

    Fixed indentation error in plotpeaks.py

--- peakplotter/plotpeaks.py
+++ peakplotter/plotpeaks.py
@@ -35,7 +35,7 @@ def read_assoc(filepath, chr_col, pos_col, pval_col, maf_col, rs_col, a1_col, a2
                                 a2_col: str
                             })
     for chunk in assoc:
-	chunk = chunk[[chr_col, pos_col, pval_col, maf_col, rs_col, a1_col, a2_col]]
+        chunk = chunk[[chr_col, rs_col, pos_col, a1_col, a2_col, maf_col, pval_col]]
         chunk[pval_col] = pd.to_numeric(chunk[pval_col], errors = 'coerce')
         nan_list = list(pd.isna(chunk[pval_col]))
         if nan_list.count(True):
